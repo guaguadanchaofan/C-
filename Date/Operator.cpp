ec760f1e9ed42a11b59197397827426f630f0a30
guaguadanchaofan <2323398255@qq.com>
2023-12-24 21:44:47 +0800

    BUG修复

--- Date/Operator.cpp
+++ Date/Operator.cpp
@@ -65,7 +65,7 @@ public:
 	int GetmonthDay(int year,int month)
 	{
 		int arr[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
-		if ((month==2)&&year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+		if (month==2&&(year % 4 == 0 && year % 100 != 0 || year % 400 == 0))
 		{
 			return 29;
 		}
