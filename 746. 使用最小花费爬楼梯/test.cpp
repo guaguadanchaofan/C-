7eec935544ffde7a85bc8ce9ab3914df9cfd8616
guaguadanchaofan <2323398255@qq.com>
2024-03-16 21:43:00 +0800

    动态规划

---
+++ 746. 使用最小花费爬楼梯/test.cpp
@@ -0,0 +1,13 @@
+#define  _CRT_SECURE_NO_WARNINGS
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1);
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[n];
+    }
+};
