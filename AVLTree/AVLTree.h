3b8c2bf64ffef245ef1331ed6ec5880855972d6a
guaguadanchaofan <2323398255@qq.com>
2024-03-31 20:09:07 +0800

    avltree

--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<iostream>
 #include<utility>
+#include <string>
+
 namespace guagua
 {
 	template<class K,class V>
@@ -28,6 +30,7 @@ namespace guagua
 	public:
 		bool Insert(const std::pair<K, V>& kv)
 		{
+			//如果为空树创建节点插入
 			if (_root == nullptr)
 			{
 				_root= new node(kv);
@@ -35,6 +38,7 @@ namespace guagua
 			}
 			node* parent = nullptr;
 			node* cur = _root;
+			//找到kv对应的位置
 			while (cur)
 			{
 				if (cur->_kv.first < kv.first)
@@ -52,9 +56,49 @@ namespace guagua
 					return false;
 				}
 			}
+			cur = new node * (kv);
+			//利用父节点找到cur节点的位置插入
+			if (parent->_kv.first < kv.first)
+			{
+				parent->_right = cur;
+				cur->_parent = parent;
+			}
+			else
+			{
+				parent->_left = cur;
+				cur->_parent = parent;
+			}
+			//更新平衡因子
+			while (parent)
+			{
+				//在左边bf--
+				if (cur == parent->_left)
+				{
+					parent->_bf--;
+				}
+				//在右边bf++
+				else
+				{
+					parent->_bf++;
+				}
+				if (parent->_bf == 0)
+				{
+					break;
+				}
+				else if (parent->_bf == 1 || parent->_bf == -1)
+				{
+					cur = parent;
+					parent = parent->_parent;
+				}
+				else if (parent->_bf == 2 || parent->_bf == -2)
+				{
+
+				}
+			}
 		}
+
 	private:
-		node* _root;
+		node* _root = nullptr;
 	};
 }
 
--- AVLTree/test.cpp
+++ AVLTree/test.cpp
@@ -1,7 +1,12 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include"AVLTree.h"
-
+using namespace std;
 int main()
 {
+	guagua::AVLTree<int, string> vk;
+	vk.Insert(pair<int, string>(2, "two"));
+	vk.Insert(pair<int, string>(3, "three"));
+	vk.Insert(pair<int, string>(4, "four"));
+	vk.Insert(pair<int, string>(5, "five"));
 	return 0;
 }
